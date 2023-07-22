7517e05cd3cb532b87daa69e47fa6a438ef0d4a8
José <josejr017@gmail.com>
2023-07-21 21:13:33 -0300

    entity food e seu controller

---
+++ src/main/java/com/example/cardapio/entities/Food.java
@@ -0,0 +1,19 @@
+package com.example.cardapio.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity(name= "foods")
+@Table(name= "foods")
+public class Food {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String image;
+	private Integer price;
+}
